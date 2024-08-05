be7a6d602b13a83a41438ca8fd6963c9768b96d8
Jmorrissette1 <J.morrissette47@gmail.com>
2024-08-05 16:17:18 -0700

    added logo

--- jason-portfolio/src/app/Nav.jsx
+++ jason-portfolio/src/app/Nav.jsx
@@ -1,13 +1,14 @@
 import styles from './page.module.css';
+import Image from "next/image";
 
 const Nav = () => {
     return(
     <div className={styles["nav-bar"]}>
         <ul>
-            <li>JM Devs</li>
+            <li><Image src="Logo-Img.svg" alt="JM logo" width={200} height={100} /></li>
             <li>About</li>
             <li>Projects</li>
-            <li>Contatct</li>
+            <li>Contact</li>
         </ul>
     </div>
     )
