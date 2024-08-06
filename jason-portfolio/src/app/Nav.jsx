512a657f4df3ad1c73519f683ffca17f2173a458
Jmorrissette1 <J.morrissette47@gmail.com>
2024-08-06 13:30:33 -0700

    styled desktop nav

--- jason-portfolio/src/app/Nav.jsx
+++ jason-portfolio/src/app/Nav.jsx
@@ -4,11 +4,12 @@ import Image from "next/image";
 const Nav = () => {
     return(
     <div className={styles["nav-bar"]}>
-        <ul>
-            <li><Image src="Logo-Img.svg" alt="JM logo" width={200} height={100} /></li>
-            <li>About</li>
-            <li>Projects</li>
-            <li>Contact</li>
+        <ul className={styles["nav-list"]}>
+            <li><Image src="Logo-Img.svg" alt="JM logo" width={120} height={100} /></li>
+            <li className={styles["nav-item"]}>About</li>
+            <li className={styles["nav-item"]}>Projects</li>
+            <li className={styles["nav-item"]}>Contact</li>
+            <li className={styles["nav-item"]}>Social Links</li>
         </ul>
     </div>
     )
