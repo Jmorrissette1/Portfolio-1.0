db5757b59f75568f9da3259bef9ec2e6258f8501
Jmorrissette1 <J.morrissette47@gmail.com>
2024-08-08 15:47:02 -0700

    added about component

---
+++ jason-portfolio/src/app/About.jsx
@@ -0,0 +1,10 @@
+import styles from './page.module.css';
+import Image from "next/image";
+
+const About = () => {
+    return(
+    <h1>About Me</h1>
+    )
+  };
+  
+  export default About;
--- jason-portfolio/src/app/page.tsx
+++ jason-portfolio/src/app/page.tsx
@@ -2,13 +2,15 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import Hero from "./Hero";
 import Nav from "./Nav";
+import About from "./About";
 
 
 export default function Home() {
   return (
    <main>
        <Nav />
-       <Hero />
+       <Hero /> 
+       <About />
    </main>
   );
 }
