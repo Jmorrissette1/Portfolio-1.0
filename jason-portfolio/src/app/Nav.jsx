ce2ff7b76a7943e5a252839209019471153fdb3a
Jmorrissette1 <J.morrissette47@gmail.com>
2024-09-08 19:59:27 -0700

    added links in nav TODO: Build Mobile Nav

--- jason-portfolio/src/app/Nav.jsx
+++ jason-portfolio/src/app/Nav.jsx
@@ -11,9 +11,9 @@ const Nav = () => {
             <li className={styles["nav-item"]}>Contact</li>
             <li className={styles["nav-item"]}>
                 <ul className={styles["social-box"]}>
-                    <li className={styles["social-icon"]}><Image src="Git-Hub-Black.svg" alt="Github Logo" width={40} height={40}/></li>
-                    <li className={styles["social-icon"]}><Image src="LinkedIn-Black.svg" alt="Github Logo" width={40} height={40}/></li>
-                    <li className={styles["social-icon"]}><Image src="X-black.svg" alt="Github Logo" width={40} height={40}/></li>
+                    <li className={styles["social-icon"]}><a href="https://github.com/Jmorrissette1"><Image src="Git-Hub-Black.svg" alt="Github Logo" width={40} height={40}/></a></li>
+                    <li className={styles["social-icon"]}><a href="https://www.linkedin.com/in/jasonmorrissette/"><Image src="LinkedIn-Black.svg" alt="Github Logo" width={40} height={40}/></a></li>
+                    <li className={styles["social-icon"]}><a href="https://x.com/JasonMorrisset8"><Image src="X-black.svg" alt="Github Logo" width={40} height={40}/></a></li>
                 </ul>
             </li>
         </ul>
