b4a18692e29b5ec52695c1c64b94f61b92f0b2e6
Jason Morrissette <j.morrissette47@gmail.com>
2024-09-23 08:36:58 -0700

    added project seciton

--- src/app/Projects.jsx
+++ src/app/Projects.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const Projects = () => {
     return(
-    <div className="featured-project-section">
+    <div className={styles["featured-project-section"]}>
         <h1>Featured Project</h1>
         <div ClassName={styles["featured-project-card"]}>
            <Image src='/img/Klean-Freak-Img.png' alt ="project 1 image" width={1200} height={600}></Image> 
