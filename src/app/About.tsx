e983e278f6786101bbd99244c2e9730860e9bc0a
Jason Morrissette <j.morrissette47@gmail.com>
2024-10-18 14:11:39 -0700

    added about section and photo

--- src/app/About.tsx
+++ src/app/About.tsx
@@ -1,12 +1,30 @@
-import styles from './page.module.css';
+import styles from "./page.module.css";
 import Image from "next/image";
 
 const About = () => {
-    return(
-      <div className={styles["about-section"]}>
-        <h1 className={styles["about-h1"]}>Your Vision, My Mission</h1>
-     </div>
-    )
-  };
-  
-  export default About;
+  return (
+    <div className={styles["about-section"]}>
+      <h1 className={styles.aboutH1}>Your Vision, My Mission</h1>
+      <div className={styles.blueLine}></div>
+      <div className={`${styles.row} ${styles.aboutContent}`}>
+        <p className={styles.aboutP}>
+          Hi, I’m Jason Morrissette, a fun-loving, nerdy designer and full-stack
+          engineer. I specialize in crafting custom web solutions using HTML,
+          CSS, JavaScript, and Next.js. With a passion for blending creativity
+          and technical prowess, I transform my clients' visions into stunning
+          and functional digital experiences. From sleek websites to dynamic web
+          applications, I’m dedicated to delivering top-notch results that
+          exceed expectations.
+        </p>
+        <Image
+          src="/img/portfolio-photo-1.jpg"
+          alt="Jason Morrissette"
+          width={500}
+          height={375}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default About;
